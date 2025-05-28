03a2eed19915015a1d4ef0fc85f86fed227ade19
Jus <julia.rosa.brito@escola.pr.gov.br>
2025-05-28 23:47:05 +0000

    Criei um arquivo chamado "app.js" para poder dar continuação na criação de cards pelo JavaScript.

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,17 +1,35 @@
-criaCartao (
-    categoria,
-    pergunta,
-    resposta
+criaCartao(
+    'Música',
+    'Quais são os tipos de instrumentos?',
+    'percussão, metais, sopros, teclado e cordas.'
 )
 
-criaCartao (
-    categoria,
-    pergunta,
-    resposta
+criaCartao(
+    'Astronomia',
+    'Quais são os planetas d oSistema Solar?',
+    'Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno.'
 )
 
-criaCartao (
-    categoria,
-    pergunta,
-    resposta
+criaCartao(
+    'Geografia',
+    'Como é formado um país?',
+    'Através uma população permanente, um território definido, um governo e a capacidade de estabelecer relações com outros países.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são rios voadores?',
+    'Correntes de vapor de água que se movem pela atmosfera, transportando umidade da Amazônia para outras partes do Brasil e da América do Sul.'
+)
+
+criaCartao(
+    'Química',
+    'Quantos elementos há na tabela periódica?',
+    '118 elementos.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um átomo?',
+    'É a unidade indivisível da matéria.'
 )
