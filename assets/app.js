8237b0b42f7a60713fa214f044a8575fb83d5a36
Jus <julia.rosa.brito@escola.pr.gov.br>
2025-05-29 01:33:34 +0000

    Adicionei uma forma que fizesse com que os cards virassem com um clique.

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,28 @@
-function criaCartao(categoria, pergunta, resposta)  {
-     console.log(categoria, pergunta, resposta)
-}
+function criaCartão(categoria, pergunta, resposta)  {
+     let container = document.getElementById('container')
+     let cartao = document.createElement('article')
+     cartao.className = 'cartão'
+
+    cartao.innerHTML = `
+    <div class="cartão__conteúdo">
+    <h3>${categoria}</h3>
+    <div class="cartão__conteúdo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartão__conteúdo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+     `
+
+     let respostaEstaVisivel = false
+
+     function viraCartão () {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+     }
+     cartao.addEventListener('click', viraCartão)
+
+     container.appendChild(cartao)
+
+    }
